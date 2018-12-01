fa02d29d7364c3a50a053fa9ab322d8285ead4da
Wade Penistone <wadespackages@yahoo.com>
2018-12-01 12:38:04 +0000

    Fix for defaults

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ module.exports = class Data
 {
   constructor(host = null, port = null)
   {
+    let {defaults} = this;
     this.host = host || process.env['DATA_HOST'] || defaults.host;
     this.port = port || process.env['DATA_PORT'] || defaults.port;
   }
